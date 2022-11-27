6fd1b7f130a0a9d7cbc26dbe69af9dfc86e78be1
juliev1234 <112874512+juliev1234@users.noreply.github.com>
2022-11-27 21:16:06 +0100

    Delete A4_FinalProject/BIMTool directory

--- A4_FinalProject/BIMTool/Office.py
+++
@@ -1,3 +0,0 @@
-import main as HTMLGenerator
-
-HTMLGenerator.modelLoader("Office_A_20110811_optimized")
--- A4_FinalProject/BIMTool/output/js/Dropdown.js
+++
@@ -1,8 +0,0 @@
-
- $(document).ready(function(){
-  $("#name").on('change', function(){
-      $(".data").hide();
-      $("#" + $(this).val()).fadeIn(700);
-  }).change();
-});
-  
